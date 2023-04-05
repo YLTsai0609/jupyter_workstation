212df54e76f82e4231a069fe016480bc47208a0b
joetsai <joetsai@17.media>
2023-04-05 23:07:39 +0800

    fix: add agent forwaring to login_gce

--- gcloud/login_gce.sh
+++ gcloud/login_gce.sh
@@ -2,3 +2,6 @@ INSTANCE_NAME=$1
 PROJECT=$2
 
 gcloud compute ssh $INSTANCE_NAME --zone asia-east1-a --project $PROJECT
+
+# If agent forwarding
+# gcloud compute ssh --ssh-flag="-A" $INSTANCE_NAME --zone asia-east1-a --project $PROJECT
